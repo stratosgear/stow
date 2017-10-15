b30dfdb470364f257493f49bb80cd8edb88d1195
Stratos Gerakakis <stratosgear@gmail.com>
2017-10-15 14:29:26 +0200

    Updates to `docked` config

--- autorandr/.config/autorandr/docked/set_xrandr.sh
+++ autorandr/.config/autorandr/docked/set_xrandr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xrandr --output DP2-3 -o left --pos 0x0
-xrandr --output DP2-2 --auto --pos 1600x0 --primary
-xrandr --output eDP1 --auto --pos 3520x0
+xrandr --output DP2-3 --mode 1600x900  --pos    0x540
+xrandr --output DP2-2 --mode 1920x1200 --pos 1600x240   --primary
+xrandr --output eDP1  --mode 2560x1440 --pos 3520x0
