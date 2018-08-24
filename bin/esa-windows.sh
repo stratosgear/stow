cc5274958797b5823006fb6ec58e033c48c38e1b
Stratos Gerakakis <stratosgear@gmail.com>
2018-08-25 00:17:10 +0300

    Change resolution of the ESA Windows rdesktop connection

--- bin/esa-windows.sh
+++ bin/esa-windows.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-rdesktop -5 -K -r clipboard:CLIPBOARD -r disk:usb=/run/media/me/LIVE sciops-ts2.net4.lan -g 1800x1000 -a 16 &
+#rdesktop -5 -K -r clipboard:CLIPBOARD -r disk:usb=/run/media/me/LIVE sciops-ts2.net4.lan -g 1800x1000 -a 16 &
+rdesktop -5 -K -r clipboard:CLIPBOARD -r disk:usb=/run/media/me/LIVE sciops-ts2.net4.lan -g 1150x960 -a 16 &
